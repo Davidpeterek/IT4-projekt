13a75dd0b1b9a75be233238ac2903d04e67af75f
Davidpeterek <david.peterek@email.cz>
2024-12-16 06:37:19 +0100

    prepsani

--- compile.sh
+++ compile.sh
@@ -5,5 +5,6 @@
 # DESCRIPTION:
 # 
 # script using pyinstaller to create executable file
+# skript používající pyinstallet to
 #
 pyinstaller --collect-all pyfiglet --onefile --add-data 'style.tcss:.' --name 'sports' sports_tui.py
