4a03aa4bbced9d863e3b961db9b18747effe826f
Davidpeterek <david.peterek@email.cz>
2024-12-16 06:44:39 +0100

    uprava

--- compile.sh
+++ compile.sh
@@ -3,8 +3,7 @@
 # BY: chadless1
 #
 # DESCRIPTION:
-# 
-# script using pyinstaller to create executable file
+#
 # skript používající pyinstallet k vytvoření spustitelného souboru
 #
 pyinstaller --collect-all pyfiglet --onefile --add-data 'style.tcss:.' --name 'sports' sports_tui.py
